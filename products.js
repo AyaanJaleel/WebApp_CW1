851c16f89f9aaf140ae0951f4a8befbb26959ee2
Ayaan Jaleel <ayaanjaleel@Ayaans-MacBook-Pro.local>
2020-11-08 00:37:31 +0400

    Adding navbar with sort options

---
+++ products.js
@@ -0,0 +1,30 @@
+let subjects = [
+    {
+        id: 1001,
+        title: "Math Club",
+        price:'300 DHS',
+        img:'https://www.seattleu.edu/media/college-of-science-and-engineering/images/undergraduatedepartments/mathematics/Math-Club-Logo-370x243.PNG',
+        stock: 5
+    },
+    {
+        id: 1002,
+        title: "English Club",
+        price:'100 DHS',
+        img:'https://www.seattleu.edu/media/college-of-science-and-engineering/images/undergraduatedepartments/mathematics/Math-Club-Logo-370x243.PNG',
+        stock: 5
+    },
+    {
+        id: 1003,
+        title: "Chemistry Club",
+        price:'250 DHS',
+        img:'https://www.seattleu.edu/media/college-of-science-and-engineering/images/undergraduatedepartments/mathematics/Math-Club-Logo-370x243.PNG',
+        stock: 5
+    },
+    {
+        id: 1004,
+        title: "Physics Club",
+        price:'200 DHS',
+        img:'https://www.seattleu.edu/media/college-of-science-and-engineering/images/undergraduatedepartments/mathematics/Math-Club-Logo-370x243.PNG',
+        stock: 5
+    }
+];
