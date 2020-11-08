517d29d4ade6556b24181afcdb863f87f0530de3
Ayaan Jaleel <ayaanjaleel@Ayaans-MacBook-Pro.local>
2020-11-09 03:32:16 +0400

    Adding cart list

--- products.js
+++ products.js
@@ -4,83 +4,95 @@ let subjects = [
         title: "Math Club",
         price: 500,
         img:'https://edtechmagazine.com/k12/sites/edtechmagazine.com.k12/files/styles/cdw_hero/public/articles/EdTech/201708/9george_math.jpg?itok=2HKdsRtP',
-        stock: 8
+        stock: 0,
+        location: "Abu Shagara"
     },
     {
         id: 1002,
         title: "English Club",
         price: 100,
         img:'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/c1a8f867794621.5b464c1e80279.png',
-        stock: 6
+        stock: 5,
+        location: "Bur Dubai"
     },
     {
         id: 1003,
         title: "Chemistry Club",
         price: 250 ,
         img:'https://openlab.citytech.cuny.edu/recreational-/files/2014/12/cc_logo.00013-1.png',
-        stock: 5
+        stock: 5,
+        location: "Masqoor"
     },
     {
         id: 1004,
         title: "Physics Club",
         price: 200 ,
         img:'https://shsoracle.org/wp-content/uploads/2017/11/Screen-Shot-2017-11-10-at-2.38.40-PM.png',
-        stock: 2
+        stock: 5,
+        location: "Ras Al Khaimah"
     },
     {
         id: 1005,
         title: "Music Club",
         price: 50,
         img:'https://image.freepik.com/free-vector/music-club-neon-logo-sign-emblem-symbol-poster_118419-90.jpg',
-        stock: 8
+        stock: 5,
+        location: "Umm Al Quwain"
     },
     {
         id: 1006,
         title: "Accountancy Club",
         price: 300 ,
         img:'https://accountantsmillionaire.club/wp-content/uploads/Accountants-Millionaires-Club-Main-Logo.png',
-        stock: 6
+        stock: 5,
+        location: "Knowledge Village"
     },
     {
         id: 1007,
         title: "Programming Club",
         price: 450 ,
         img:'https://www.mineplex.com/data/teams/covers/0/79.jpg?1536867642',
-        stock: 5
+        stock: 1,
+        location: "Burjuman"
     },
     {
         id: 1008,
         title: "Scientology Club",
         price: 20,
         img:'https://www.mikerindersblog.org/wp-content/uploads/2017/03/scientology-3.jpg',
-        stock: 2
+        stock: 2,
+        location: "Nahda"
     },
     {
         id: 1009,
         title: "History Club",
         price: 350,
         img:'https://nphistoryclub.weebly.com/uploads/6/1/6/1/61611671/history_club_logo_2.png',
-        stock: 5
+        stock: 5,
+        location: "Qiyadah"
     },
     {
         id: 1010,
         title: "PE Club",
         price: 200,
         img:'https://bloximages.newyork1.vip.townnews.com/hickoryrecord.com/content/tncms/assets/v3/editorial/3/1c/31c95f06-7907-5d67-9b64-57dee8eb4bfc/58dd975f3d7ac.image.jpg?resize=1024%2C782',
-        stock: 2
+        stock: 4,
+        location: "Abu Bakr Al Siddique"
     },
     {
         id: 1011,
         title: "Law Club",
         price: 650,
         img:'https://lh3.googleusercontent.com/proxy/xef7g9bzUIZFa_aA8ngZRA-DNRNfQc96YAXXi2UYKSpn4l918jtlxP6W0dNeyythJEaeLM-XZ1Sz500jURPmGyJndiRt1dBYUQ9uTsWXOiyroUI0WitQuXEJa8UluQI',
-        stock: 5
+        stock: 3,
+        location: "Barsha"
     },
     {
         id: 1012,
         title: "Entrepreneurship Club",
         price: 150,
         img:'https://pbs.twimg.com/profile_images/1539897372/ec_logo.jpg',
-        stock: 5
+        stock: 3,
+        location: "Jafaliya"
     },
 ];
