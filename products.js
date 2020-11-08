4b5350425885ebb714efbd38a7d1196c525abf4a
Ayaan Jaleel <ayaanjaleel@Ayaans-MacBook-Pro.local>
2020-11-08 04:00:24 +0400

    Adding sort functions

--- products.js
+++ products.js
@@ -2,29 +2,85 @@ let subjects = [
     {
         id: 1001,
         title: "Math Club",
-        price:'300 DHS',
+        price: 500,
         img:'https://edtechmagazine.com/k12/sites/edtechmagazine.com.k12/files/styles/cdw_hero/public/articles/EdTech/201708/9george_math.jpg?itok=2HKdsRtP',
-        stock: 5
+        stock: 8
     },
     {
         id: 1002,
         title: "English Club",
-        price:'100 DHS',
-        img:'https://edtechmagazine.com/k12/sites/edtechmagazine.com.k12/files/styles/cdw_hero/public/articles/EdTech/201708/9george_math.jpg?itok=2HKdsRtP',
-        stock: 5
+        price: 100,
+        img:'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/c1a8f867794621.5b464c1e80279.png',
+        stock: 6
     },
     {
         id: 1003,
         title: "Chemistry Club",
-        price:'250 DHS',
-        img:'https://www.seattleu.edu/media/college-of-science-and-engineering/images/undergraduatedepartments/mathematics/Math-Club-Logo-370x243.PNG',
+        price: 250 ,
+        img:'https://openlab.citytech.cuny.edu/recreational-/files/2014/12/cc_logo.00013-1.png',
         stock: 5
     },
     {
         id: 1004,
         title: "Physics Club",
-        price:'200 DHS',
-        img:'https://www.seattleu.edu/media/college-of-science-and-engineering/images/undergraduatedepartments/mathematics/Math-Club-Logo-370x243.PNG',
+        price: 200 ,
+        img:'https://shsoracle.org/wp-content/uploads/2017/11/Screen-Shot-2017-11-10-at-2.38.40-PM.png',
+        stock: 2
+    },
+    {
+        id: 1005,
+        title: "Music Club",
+        price: 50,
+        img:'https://image.freepik.com/free-vector/music-club-neon-logo-sign-emblem-symbol-poster_118419-90.jpg',
+        stock: 8
+    },
+    {
+        id: 1006,
+        title: "Accountancy Club",
+        price: 300 ,
+        img:'https://accountantsmillionaire.club/wp-content/uploads/Accountants-Millionaires-Club-Main-Logo.png',
+        stock: 6
+    },
+    {
+        id: 1007,
+        title: "Programming Club",
+        price: 450 ,
+        img:'https://www.mineplex.com/data/teams/covers/0/79.jpg?1536867642',
         stock: 5
-    }
+    },
+    {
+        id: 1008,
+        title: "Scientology Club",
+        price: 20,
+        img:'https://www.mikerindersblog.org/wp-content/uploads/2017/03/scientology-3.jpg',
+        stock: 2
+    },
+    {
+        id: 1009,
+        title: "History Club",
+        price: 350,
+        img:'https://nphistoryclub.weebly.com/uploads/6/1/6/1/61611671/history_club_logo_2.png',
+        stock: 5
+    },
+    {
+        id: 1010,
+        title: "PE Club",
+        price: 200,
+        img:'https://bloximages.newyork1.vip.townnews.com/hickoryrecord.com/content/tncms/assets/v3/editorial/3/1c/31c95f06-7907-5d67-9b64-57dee8eb4bfc/58dd975f3d7ac.image.jpg?resize=1024%2C782',
+        stock: 2
+    },
+    {
+        id: 1011,
+        title: "Law Club",
+        price: 650,
+        img:'https://lh3.googleusercontent.com/proxy/xef7g9bzUIZFa_aA8ngZRA-DNRNfQc96YAXXi2UYKSpn4l918jtlxP6W0dNeyythJEaeLM-XZ1Sz500jURPmGyJndiRt1dBYUQ9uTsWXOiyroUI0WitQuXEJa8UluQI',
+        stock: 5
+    },
+    {
+        id: 1012,
+        title: "Entrepreneurship Club",
+        price: 150,
+        img:'https://pbs.twimg.com/profile_images/1539897372/ec_logo.jpg',
+        stock: 5
+    },
 ];
